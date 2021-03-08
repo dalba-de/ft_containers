4d30719b3e66b5273adabcaa53031c7f5c6a20f5
dalba-de <D.alba85@gmail.com>
2021-03-08 18:51:14 +0100

    list test finished

--- Test/ListTest.cpp
+++ Test/ListTest.cpp
@@ -1,4 +1,5 @@
 #include <list>
+#include <cmath>
 #include "../List/List.hpp"
 #include "../Tools/Shared_functions.hpp"
 # define RESET		"\033[0m"
@@ -28,6 +29,28 @@ foo::~foo()
 {
 }
 
+// compare only integral part:
+bool mycomparison (double first, double second)
+{ return ( int(first)<int(second) ); }
+
+// a predicate implemented as a function:
+bool single_digit (const int& value) 
+{ return (value<10); }
+
+// a predicate implemented as a class:
+struct is_odd {
+bool operator() (const int& value) { return (value%2)==1; }
+};
+
+// a binary predicate implemented as a function:
+bool same_integral_part (double first, double second)
+{ return ( int(first)==int(second) ); }
+
+// a binary predicate implemented as a class:
+struct is_near {
+  bool operator() (double first, double second)
+  { return (fabs(first-second)<5.0); }
+};
 
 template <class T>
 void	printLists(std::list<T> list, ft::List<T> mylist)
@@ -36,6 +59,18 @@ void	printLists(std::list<T> list, ft::List<T> mylist)
 	ft::printoList(list);
 	std::cout << GREEN"\nMy List output:\n" << RESET << std::endl;
 	ft::printList(mylist);
+}
+
+bool compare_nocase (const std::string& first, const std::string& second)
+{
+  unsigned int i=0;
+  while ( (i<first.length()) && (i<second.length()) )
+  {
+    if (tolower(first[i])<tolower(second[i])) return true;
+    else if (tolower(first[i])>tolower(second[i])) return false;
+    ++i;
+  }
+  return ( first.length() < second.length() );
 }
 
 void	printBanner()
@@ -61,6 +96,263 @@ void	checkContinue()
 		std::getline(std::cin, input);
 	}
 	printBanner();
+}
+
+void	relationalOperatorsTest()
+{
+	std::cout << RED"Starting relational operators test" << RESET << std::endl;
+	std::cout << BLUE"\nCreating three int list:\n" << RESET << std::endl;
+	int	intsa[] = {10, 20, 30};
+	int	intsb[] = {30, 20, 10};	
+	std::list<int> a(intsa, intsa + 3);
+	std::list<int> b(intsa, intsa + 3);
+	std::list<int> c(intsb, intsb + 3);
+	ft::List<int> mya(intsa, intsa + 3);
+	ft::List<int> myb(intsa, intsa + 3);
+	ft::List<int> myc(intsb, intsb + 3);
+	std::cout << BLUE"Print a: " << std::endl;
+	printLists(a, mya);
+	std::cout << BLUE"Print b: " << std::endl;
+	printLists(b, myb);
+	std::cout << BLUE"Print c: " << std::endl;
+	printLists(c, myc);
+
+	std::cout << ORANGE"\n\nChecking relational operators:" << RESET << std::endl;
+	std::cout << GREEN"\n\nList output:" << RESET << std::endl;
+	if (a==b) std::cout << "a and b are equal\n";
+	if (b!=c) std::cout << "b and c are not equal\n";
+	if (b<c) std::cout << "b is less than c\n";
+	if (c>b) std::cout << "c is greater than b\n";
+	if (a<=b) std::cout << "a is less than or equal to b\n";
+	if (a>=b) std::cout << "a is greater than or equal to b\n";
+
+	std::cout << GREEN"\n\nMy list output:" << RESET << std::endl;
+	if (mya==myb) std::cout << "a and b are equal\n";
+	if (myb!=myc) std::cout << "b and c are not equal\n";
+	if (myb<myc) std::cout << "b is less than c\n";
+	if (myc>myb) std::cout << "c is greater than b\n";
+	if (mya<=myb) std::cout << "a is less than or equal to b\n";
+	if (mya>=myb) std::cout << "a is greater than or equal to b\n";
+
+	std::cout << ORANGE"\n\nChecking swap() function (no member overload):" << RESET << std::endl;
+	std::cout << BLUE"\nCreating two int list:\n" << RESET << std::endl;
+	std::list<int> foo (3,100);
+	std::list<int> bar (5,200);
+	ft::List<int> myfoo (3,100);
+	ft::List<int> mybar (5,200);
+	std::cout << BLUE"Print foo: " << std::endl;
+	printLists(foo, myfoo);
+	std::cout << BLUE"\nPrint bar: " << std::endl;
+	printLists(bar, mybar);
+
+	std::cout << BLUE"\nSwap lists:\n" << RESET << std::endl;
+	std::swap(foo,bar);
+	ft::swap(myfoo, mybar);
+	std::cout << BLUE"Print foo: " << std::endl;
+	printLists(foo, myfoo);
+	std::cout << BLUE"\nPrint bar: " << std::endl;
+	printLists(bar, mybar);
+}
+
+void	operationsTest()
+{
+	std::cout << RED"Starting operations test" << RESET << std::endl;
+	std::cout << ORANGE"\n\nChecking splice() function:" << RESET << std::endl;
+	std::cout << BLUE"\nSet some initial values to first and second list:\n" << RESET << std::endl;
+	std::list<int> first;
+  	std::list<int> second;
+	ft::List<int> myfirst;
+  	ft::List<int> mysecond;
+	std::list<int>::iterator it;
+	ft::List<int>::iterator myit;
+	for (int i=1; i<=4; ++i)
+	{
+		first.push_back(i);
+		myfirst.push_back(i);
+	}
+	for (int i=1; i<=3; ++i)
+	{
+		second.push_back(i*10);
+		mysecond.push_back(i*10);
+	}
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << BLUE"\nPrint second: " << std::endl;
+	printLists(second, mysecond);
+    
+	it = first.begin();
+	it++;
+	myit = myfirst.begin();
+	myit++;
+    std::cout << BLUE"\nCreate iterator pointing to 2, and splice second in first (entire list):\n" << RESET << std::endl;
+	first.splice(it, second);
+	myfirst.splice(myit, mysecond);
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << BLUE"\nPrint second: " << std::endl;
+	printLists(second, mysecond);
+
+	std::cout << BLUE"\nTransferring 2 into second (single element):\n" << RESET << std::endl;
+	second.splice(second.begin(), first, it);
+	mysecond.splice(mysecond.begin(), myfirst, myit);
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << BLUE"\nPrint second: " << std::endl;
+	printLists(second, mysecond);
+
+	std::cout << BLUE"\nPointing iterator to '30' and moving elements (range element):\n" << RESET << std::endl;
+	it = first.begin();
+	std::advance(it, 3);
+	myit = myfirst.begin();
+	std::advance(myit, 3);
+	first.splice(first.begin(), first, it, first.end());
+	myfirst.splice(myfirst.begin(), myfirst, myit, myfirst.end());
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+
+	std::cout << ORANGE"\n\nChecking remove() function:" << RESET << std::endl;
+	std::cout << BLUE"\nRemoving '30' and '4':\n" << RESET << std::endl;
+	first.remove(30);
+	first.remove(4);
+	myfirst.remove(30);
+	myfirst.remove(4);
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+
+	std::cout << ORANGE"\n\nChecking remove_if() function:" << RESET << std::endl;
+	int myints[]= {15,36,7,17,20,39,4,1};
+	std::cout << BLUE"\nAssign {15,36,7,17,20,39,4,1} to first:\n" << RESET << std::endl;
+	first.assign(myints, myints + 8);
+	myfirst.assign(myints, myints + 8);
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+
+	std::cout << BLUE"\nTry remove single-digit values:\n" << RESET << std::endl;
+	first.remove_if(single_digit);
+	myfirst.remove_if(single_digit);
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+
+	std::cout << BLUE"\nTry remove odd values:\n" << RESET << std::endl;
+	first.remove_if(is_odd());
+	myfirst.remove_if(is_odd());
+	std::cout << BLUE"Print first: " << std::endl;
+	printLists(first, myfirst);
+
+	std::cout << ORANGE"\n\nChecking unique() function:" << RESET << std::endl;
+	std::cout << BLUE"\nCreate double list with { 12.15,  2.72, 73.0,  12.77,  3.14, \
+12.77, 73.35, 72.25, 15.3,  72.25 } values:\n" << RESET << std::endl;
+	double mydoubles[]={ 12.15,  2.72, 73.0,  12.77,  3.14,
+                       12.77, 73.35, 72.25, 15.3,  72.25 };
+	std::list<double>	third(mydoubles, mydoubles+10);
+	ft::List<double>	mythird(mydoubles, mydoubles+10);
+	std::cout << BLUE"Print third: " << std::endl;
+	printLists(third, mythird);
+
+	std::cout << BLUE"\nSorting list and remove equal values:\n" << RESET << std::endl;
+	third.sort();
+	third.unique();
+	mythird.sort();
+	mythird.unique();
+	std::cout << BLUE"Print third: " << std::endl;
+	printLists(third, mythird);
+
+	std::cout << BLUE"\nRemove values with same integral part:\n" << RESET << std::endl;
+	third.unique(same_integral_part);
+	mythird.unique(same_integral_part);
+	std::cout << BLUE"Print third: " << std::endl;
+	printLists(third, mythird);
+
+	std::cout << BLUE"\nRemove values with difference < 5.0:\n" << RESET << std::endl;
+	third.unique(is_near());
+	mythird.unique(is_near());
+	std::cout << BLUE"Print third: " << std::endl;
+	printLists(third, mythird);
+
+	std::cout << ORANGE"\n\nChecking merge() function:" << RESET << std::endl;
+	std::cout << BLUE"\nCreate double list fourth with {3.1, 2.2, 2.9} values and list five with {3.7, 7.1, 1.4} values:\n" << RESET << std::endl;
+	std::list<double>	fourth;
+	std::list<double>	five;
+	ft::List<double>	myfourth;
+	ft::List<double>	myfive;
+	fourth.push_back (3.1);
+	fourth.push_back (2.2);
+	fourth.push_back (2.9);
+	five.push_back (3.7);
+	five.push_back (7.1);
+	five.push_back (1.4);
+	myfourth.push_back (3.1);
+	myfourth.push_back (2.2);
+	myfourth.push_back (2.9);
+	myfive.push_back (3.7);
+	myfive.push_back (7.1);
+	myfive.push_back (1.4);
+	std::cout << BLUE"Print fourth: " << std::endl;
+	printLists(fourth, myfourth);
+	std::cout << BLUE"\nPrint five: " << std::endl;
+	printLists(five, myfive);
+
+	std::cout << BLUE"\nSorting lists and merge five into fourth:\n" << RESET << std::endl;
+	fourth.sort();
+	five.sort();
+	myfourth.sort();
+	myfive.sort();
+	fourth.merge(five);
+	myfourth.merge(myfive);
+	std::cout << BLUE"Print fourth: " << std::endl;
+	printLists(fourth, myfourth);
+	std::cout << BLUE"\nPrint five: " << std::endl;
+	printLists(five, myfive);
+
+	std::cout << BLUE"\nPush '2.1' in five and merge comparing only integral part:\n" << RESET << std::endl;
+	five.push_back(2.1);
+	myfive.push_back(2.1);
+	fourth.merge(five, mycomparison);
+	myfourth.merge(myfive, mycomparison);
+	std::cout << BLUE"Print fourth: " << std::endl;
+	printLists(fourth, myfourth);
+	std::cout << BLUE"\nPrint five: " << std::endl;
+	printLists(five, myfive);
+
+	std::cout << ORANGE"\n\nChecking sort() function:" << RESET << std::endl;
+	std::cout << BLUE"\nCreating string list with 'one', 'two' and 'Three' values:\n" << RESET << std::endl;
+	std::list<std::string> list;
+	std::list<std::string>::iterator sit;
+	list.push_back ("one");
+	list.push_back ("two");
+	list.push_back ("Three");
+	ft::List<std::string> mylist;
+	ft::List<std::string>::iterator mysit;
+	mylist.push_back ("one");
+	mylist.push_back ("two");
+	mylist.push_back ("Three");
+
+	std::cout << BLUE"\nSorting list:\n" << RESET << std::endl;
+	list.sort();
+	mylist.sort();
+	printLists(list, mylist);
+
+	std::cout << BLUE"\nSorting list comparing no case sensitive:\n" << RESET << std::endl;
+	list.sort(compare_nocase);
+	mylist.sort(compare_nocase);
+	printLists(list, mylist);
+
+	std::cout << ORANGE"\n\nChecking reverse() function:" << RESET << std::endl;
+	std::cout << BLUE"\nAssign values from 1 to 9 to int list:\n" << RESET << std::endl;
+	first.clear();
+	myfirst.clear();
+	for (int i=1; i<10; ++i)
+	{
+		first.push_back(i);
+		myfirst.push_back(i);
+	}
+	std::cout << BLUE"\nPrint int list: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << BLUE"\nReversing int list: " << std::endl;
+	first.reverse();
+	myfirst.reverse();
+	printLists(first, myfirst);
+	checkContinue();
 }
 
 void	modifiersTest()
@@ -165,6 +457,48 @@ void	modifiersTest()
 	myfirst.erase(myit, myit2);
 	printLists(first, myfirst);
 
+	std::cout << ORANGE"\n\nChecking swap() function:" << RESET << std::endl;
+	std::cout << BLUE"\nSwaping content from first to second:\n" << RESET << std::endl;
+	
+	std::cout << BLUE"Print first before swap: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << GREEN"\nfirst size: " << first.size() << ". myfirst size: " << myfirst.size() << RESET << std::endl;
+	std::cout << BLUE"\nPrint second before swap: " << std::endl;
+	printLists(second, mysecond);
+	std::cout << GREEN"\nsecond size: " << second.size() << ". mysecond size: " << mysecond.size() << RESET << std::endl << std::endl;
+
+	first.swap(second);
+	myfirst.swap(mysecond);
+	std::cout << BLUE"Print first after swap: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << GREEN"\nfirst size: " << first.size() << ". myfirst size: " << myfirst.size() << RESET << std::endl;
+	std::cout << BLUE"\nPrint second after swap: " << std::endl;
+	printLists(second, mysecond);
+	std::cout << GREEN"\nsecond size: " << second.size() << ". mysecond size: " << mysecond.size() << RESET << std::endl << std::endl;
+
+	std::cout << ORANGE"\n\nChecking resize() function:" << RESET << std::endl;
+	std::cout << BLUE"\nResize first to 7, with zero-initialized values:\n" << RESET << std::endl;
+	first.resize(7);
+	myfirst.resize(7);
+	printLists(first, myfirst);
+
+	std::cout << BLUE"\nResize first to 10, with val = 42:\n" << RESET << std::endl;
+	first.resize(10, 42);
+	myfirst.resize(10, 42);
+	printLists(first, myfirst);
+
+	std::cout << BLUE"\nResize first to 5::\n" << RESET << std::endl;
+	first.resize(5);
+	myfirst.resize(5);
+	printLists(first, myfirst);
+
+	std::cout << ORANGE"\n\nChecking clear() function:" << RESET << std::endl;
+	first.clear();
+	myfirst.clear();
+	std::cout << BLUE"Print first after clear: " << std::endl;
+	printLists(first, myfirst);
+	std::cout << GREEN"\nfirst size: " << first.size() << ". myfirst size: " << myfirst.size() << RESET << std::endl;
+	checkContinue();
 }
 
 void	elementAccessTest()
@@ -358,11 +692,13 @@ void	constructorTest()
 int main()
 {
 	printBanner();
-	/*constructorTest();
+	constructorTest();
 	iteratorsTest();
 	capacityTest();
-	elementAccessTest();*/
+	elementAccessTest();
 	modifiersTest();
+	operationsTest();
+	relationalOperatorsTest();
 	
 	return 0;
 }

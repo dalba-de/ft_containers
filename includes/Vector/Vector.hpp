67ab95bcbc64463c8772c4839f563e89de43f36d
dalba-de <D.alba85@gmail.com>
2021-03-09 10:39:39 +0100

    fixed const_reverse_iterator in vector

--- includes/Vector/Vector.hpp
+++ includes/Vector/Vector.hpp
@@ -111,11 +111,11 @@ namespace ft
 
 			reverse_iterator		rbegin() { return reverse_iterator(this->end()); }
 
-			const_reverse_iterator	rbegin() const { return reverse_iterator(this->end()); }
+			const_reverse_iterator	rbegin() const { return const_reverse_iterator(this->end()); }
 
 			reverse_iterator		rend() { return (reverse_iterator(this->begin())); }
 
-			const_reverse_iterator	rend() const { return (reverse_iterator(this->begin())); }
+			const_reverse_iterator	rend() const { return (const_reverse_iterator(this->begin())); }
 
 /*
 ** ------------------------------- CAPACITY --------------------------------
